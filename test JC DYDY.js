a9a0d9d31eb9e58e58901edddbc0d79558a1fdd1
DYDYICE <info@ice-mountain.com>
2025-03-13 14:33:08 +0100

    Update test JC DYDY.js

--- test JC DYDY.js
+++ test JC DYDY.js
@@ -1,5 +1,23 @@
-var animals = [
-    ["37", "Bengal Tiger", "mammal", "carnivore", "3m"],
-    ["1", "African Elephant", "mammal", "herbivore", "3.3m"],
-    ["2", "Green Anaconda", "reptile", "carnivore", "6m"]
-];
+// Language variable (if needed)
+var taal = "nl";
+
+// ANIMALS LIST
+let animal1  =  ["1", "Ara Bleu", "bird", "jungle", "Amérique du Sud"];
+let animal2  =  ["2", "Amazone Aourou", "bird", "rainforest", "Amérique du Sud"];
+let animal3  =  ["3", "Loriquet Arc-en-ciel", "bird", "forest", "Australie"];
+let animal4  =  ["4", "Grand Eclectus", "bird", "tropical", "Océanie"];
+let animal5  =  ["5", "Kakariki", "bird", "forest", "Nouvelle-Zélande"];
+let animal6  =  ["6", "Inséparables", "bird", "savanna", "Afrique"];
+let animal7  =  ["7", "Perruche Omnicolore", "bird", "forest", "Australie"];
+let animal8  =  ["8", "Perruche Paliceps", "bird", "forest", "Australie"];
+let animal9  =  ["9", "Perruche à Collier", "bird", "jungle", "Afrique, Asie"];
+let animal10 = ["10", "Perruche ondulée", "bird", "savanna", "Australie"];
+let animal11 = ["11", "Touracos à joues blanches", "bird", "highlands", "Éthiopie"];
+let animal12 = ["12", "Martin-Chasseur", "bird", "coastal", "Australie, Tasmanie"];
+let animal13 = ["13", "Grand-Duc Africain", "bird", "savanna", "Afrique"];
+let animal14 = ["14", "Emeu d’Australie", "bird", "grassland", "Australie"];
+let animal15 = ["15", "Faisan doré", "bird", "mountains", "Asie"];
+let animal16 = ["16", "Wallaby de Bennett", "mammal", "forest", "Australie"];
+let animal17 = ["17", "Raton Laveur", "mammal", "woodlands", "Amérique du Nord"];
+let animal18 = ["18", "Cochon vietnamien", "mammal", "farmland", "Vietnam"];
+let animal19 = ["19", "Suricates", "mammal", "desert", "Afrique"];
