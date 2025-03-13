af86c6fe98b00d69f284cd48a3852f094b5d18f1
DYDYICE <info@ice-mountain.com>
2025-03-13 14:03:58 +0100

    Add files via upload

---
+++ test JC DYDY.js
@@ -0,0 +1,5 @@
+var animals = [
+    ["37", "Bengal Tiger", "mammal", "carnivore", "3m"],
+    ["1", "African Elephant", "mammal", "herbivore", "3.3m"],
+    ["2", "Green Anaconda", "reptile", "carnivore", "6m"]
+];
